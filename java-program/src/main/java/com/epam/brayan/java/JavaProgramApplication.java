973f9dcbe630cce6a18230e4a7c19c0a8186fb30
Brayan Salcedo <brayan_salcedo@epam.com>
2023-08-11 15:21:34 -0600

    initial commit

---
+++ java-program/src/main/java/com/epam/brayan/java/JavaProgramApplication.java
@@ -0,0 +1,13 @@
+package com.epam.brayan.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProgramApplication.class, args);
+	}
+
+}
---
+++ java-program/src/main/java/com/epam/brayan/java/service/UserService.java
@@ -0,0 +1,4 @@
+package com.epam.brayan.java.service;
+
+public class UserService {
+}
